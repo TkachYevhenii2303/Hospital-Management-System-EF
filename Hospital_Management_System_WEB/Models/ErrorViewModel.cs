af08748356b87ed5e0c3e049d742babb8538a87c
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-06-17 08:32:31 +0300

    Start working with React

--- Hospital_Management_System_WEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hospital_Management_System_WEB.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
